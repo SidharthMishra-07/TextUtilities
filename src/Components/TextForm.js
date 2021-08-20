0f0fcca7008c8fd9ca859a4a55f0eeeea5eb594f
Sidharth Mishra <sidharth1604@gmail.com>
2021-08-20 23:35:13 +0530

    useState hook

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -2,7 +2,13 @@ import React, {useState} from 'react'       //Here useState is a react hook
 import PropTypes from 'prop-types'
 
 export default function TextForm(props) {
-
+    const handleUppercase =()=>{
+        setText(text.toUpperCase());
+    }
+    const handleOnchange=(event) =>{
+        console.log("On Change");
+        setText(event.target.value);
+    }
     //Here text is a variable having deafult text as 'Enter text here'
     //setText is an updation variable ie; if i want update text....I'll type setText = "Text changed to this"
     const [text, setText] = useState('Enter text here');
@@ -10,9 +16,9 @@ export default function TextForm(props) {
         <div>
             <h2>{props.heading}</h2>
             <div className="mb-3">
-                <textarea className="form-control" id="myText" rows="8"></textarea>
+                <textarea value = {text} onChange={handleOnchange} className="form-control" id="myText" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary">Capitalize Text</button>
+            <button className="btn btn-primary" onClick={handleUppercase}>Capitalize Text</button>
         </div>  
     )
 }
