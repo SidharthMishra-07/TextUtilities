3cc1c75f6a2f6976a1eec56d60e5026c1472747d
Sidharth Mishra <sidharth1604@gmail.com>
2021-08-22 00:17:16 +0530

    DarkExp

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 
@@ -9,6 +10,9 @@ function App() {
     <div className="container my-3">
         <TextForm heading="Enter the text to analyze"/>
     </div>
+    <hr />
+    <About/>
+
 
     </>
   );
