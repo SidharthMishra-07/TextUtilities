0b773fce9c9801103aa76f8e8e30d9bdfae58f4d
Sidharth Mishra <sidharth1604@gmail.com>
2021-08-24 18:38:44 +0530

    DarkExp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
+import React, { useState } from 'react'
 import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 
 function App() {
   const [Mode, setMode] = useState('light') //Whether dark mode is available or not
+
+  const toggleMode=()=>{
+    if(Mode === 'light'){
+      setMode('dark');
+    }
+    else{
+      setMode('light');
+    }
+  }
   return (
     <>
-    <Navbar title="TextUtils"  mode={Mode}/>   {/*AboutText was set in default props */}
+    <Navbar title="TextUtils" mode={Mode} toggle={toggleMode}/>   {/*AboutText was set in default props */}
     <div className="container my-3">
         <TextForm heading="Enter the text to analyze"/>
     </div>
