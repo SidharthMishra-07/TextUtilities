be01350ffe83e385b0b1f514f3b092cc0d344eb8
Sidharth Mishra <sidharth1604@gmail.com>
2021-08-24 22:07:24 +0530

    AlertTimeout

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -42,7 +42,7 @@ export default function TextForm(props) {
         </div>
         <div className="container my-4" style={{color: props.mode==='dark'?'white':'#15181a'}}>
             <h2>Word Count </h2>
-            <p>{text.split(' ').filter(function(n){return n!=''}).length} words and {text.length} characters</p>
+            <p>{text.split(' ').filter(function(n){return n!==''}).length} words and {text.length} characters</p>
             
         </div>
         </> 
