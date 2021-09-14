b3cef5d367014a3f3c32a9b6ec3ef1c152e0472b
Sidharth Mishra <sidharth1604@gmail.com>
2021-09-14 15:20:01 +0530

    Changes Made

--- src/Components/About.js
+++ src/Components/About.js
@@ -68,7 +68,7 @@ export default function About(props) {
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingThree">
                         <button className="accordion-button collapsed" style={{backgroundColor: props.mode==='dark'?'#15181a':'white', color: props.mode==='dark'?'white':'#15181a'}} type ="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                            Accordion Item #3
+                            Information 3
                         </button>
                     </h2>
                     <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
